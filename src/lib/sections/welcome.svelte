7da80da592c56dd737b3ba0f46325d6300649f42
Thomas Buchholz <64580434+OKThomas1@users.noreply.github.com>
2023-07-02 14:27:15 -0700

    Update welcome.svelte

--- src/lib/sections/welcome.svelte
+++ src/lib/sections/welcome.svelte
@@ -1,5 +1,5 @@
 <div class="container">
-	<h1 class="display-6">Hi, my name is</h1>
+	<h1 class="display-6">Hello, my name is</h1>
 	<h1 class="display-1" style="color: #d9ed92"><strong>Thomas Buchholz</strong></h1>
 	<h1 class="display-4">I am a full stack software developer</h1>
 </div>
